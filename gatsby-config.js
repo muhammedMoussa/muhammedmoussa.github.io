344e0d856093e5faf5540160053f74338757ccae
Muhammed <muhammedmoussa@hotmail.com>
2020-08-12 10:43:29 +0200

    refactor: social links

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     quote: 'I\'m not a great programmer; I\'m just a good programmer with great habits; clean, solid, and tested code.',
     summary: 'A thorough few years of expertise worked in various industries like Saas,\ Booking, Event Planning, ERP, and Social platforms. with a passion for delivering high-quality software throw requirements to end-user. Regardless of the technology/framework limitation and the platform (Web, Mobile, or PWA).',
     social: [
+      { icon: 'fab fa-linkedin', url: 'https://www.linkedin.com/in/muhammedmoussa'},
       { icon: 'fab fa-twitter', url: 'http://twitter.com/muhammedMoussa'},
       { icon: 'fab fa-github', url: 'http://github.com/muhammedMoussa'},
       { icon: 'fab fa-stack-overflow', url: 'https://stackoverflow.com/story/muhammedmoussa'},
       { icon: 'fab fa-quora', url: 'https://www.quora.com/profile/Muhammed-Moussa-2'},
-      { icon: 'fab fa-linkedin', url: 'https://www.linkedin.com/in/muhammedmoussa/  '},
       { icon: 'fab fa-codepen', url: 'https://codepen.io/muhammedMoussa'},
       { icon: 'fab fa-dev', url: 'https://dev.to/muhammedmoussa'},
       { icon: 'fab fa-medium', url: 'https://medium.com/@muhammedmoussa'},
@@ -20,7 +20,7 @@ module.exports = {
       { icon: 'fab fa-bitbucket', url: 'https://bitbucket.org/muhammedmoussa/'}
     ],
     version: '2.0.3'
-  },
+  }, 
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
@@ -50,14 +50,14 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        spaceId: process.env.CONTENTFUL_SPACE_ID || 'lj21o0y4riho',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || 'MhcfymbIQnoLbZi_kpoGZ9jh0ke_qxv2Q_by5_6TxJQ'
       }
     },
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: process.env.SHORTNAME      
+        shortname: process.env.SHORTNAME || 'muhammedmoussa'     
       }
     }
   ]

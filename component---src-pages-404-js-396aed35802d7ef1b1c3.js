ba7d2c9e2be2438f9a0a13b0d9cfc25ca110492a
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-11-16 01:20:00 +0200

    Updates

---
+++ component---src-pages-404-js-396aed35802d7ef1b1c3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Wbzz"),r=n("Bl7J"),u=n("hIod");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"404"}),l.a.createElement("h1",null,"PAGE NOT FOUND"),l.a.createElement("p",null,l.a.createElement(c.a,{to:"/"},"BACK HOME")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-396aed35802d7ef1b1c3.js.map

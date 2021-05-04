689c3c4177ef7e4b12bf72d05dad3aa317d7d570
muhammedMoussa <muhammedmoussa@hotmail.com>
2021-05-05 01:31:53 +0200

    update GalleryPage ui

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Head from '../components/head'
+import Layout from '../components/layout'
 
 function GalleryPage() {
     return (
-        <div>
-            Coming...
-        </div>
+        <Layout>
+            <Head title="Gallery" />
+            <h1 className="text-4xl font-bold ">Coming...</h1>
+        </Layout>
     )
 }
 
